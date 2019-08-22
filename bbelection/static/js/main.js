bb77a3cb588321c5729776d6f58e40c620945b38
adamc66 <adam.cote66@gmail.com>
2019-08-22 15:11:06 -0400

    completed voting part 1

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    axios
+    .get('https://eric-deploy-test.herokuapp.com/bb')
+    .then(function(response){
+        console.log(response.data)
+        let mainlist = document.querySelector('#candidates')
+        let arr = response.data.candidates
+        arr.forEach(candidate =>{
+            let newCandidate = document.createElement('li')
+            newCandidate.innerText = ('Name: ' + candidate.name + ', Votes: ' + candidate.votes)
+            mainlist.appendChild(newCandidate)
+        })
+    })
 });
